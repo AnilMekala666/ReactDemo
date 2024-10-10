2cb7f816f14b1a45e93b38239b7a5f1072e823cf
Sireesha <manay@innoclique.com>
2024-10-10 15:27:06 +0530

    drawers

---
+++ src/components/payments/CustomDrawer.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Box, Drawer, Typography, CircularProgress, Button } from '@mui/material';
+import { styled } from '@mui/system';
+
+const CustomDrawer = ({ open, onClose, children, anchor = 'right', width = 300, leftOffset = 0, top, title,
+    content,
+    button, }) => {
+
+    return (
+        <Drawer
+            anchor="right"
+            open={open}
+            onClose={onClose}
+            PaperProps={{
+                sx: {
+                    width: width,
+                    height: 'auto',
+                    position: 'fixed', // Fix the position
+                    right: `${leftOffset}px`, // Offset to move the drawer to the left
+                    top: top,
+                    transform: 'translateY(-50%)',
+                    transition: 'transform 0.3s ease, right 0.3s ease', // Smooth transition
+                    maxHeight: '90vh', // Optional: limit max height
+                    zIndex: 1300,
+                }
+            }}
+        >
+            <Box role="presentation" sx={{ p: 2 }}>
+                <Typography variant="h6">{title}</Typography>
+                <Box>{content}</Box>
+                {button}
+            </Box>
+        </Drawer>
+    );
+}
+
+export default CustomDrawer
